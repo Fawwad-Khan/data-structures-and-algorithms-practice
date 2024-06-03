e043e2ea12eb915e3ba55ba3fbd452fa3ff283ea
Fawwad-Khan <fawwad.j4k@gmail.com>
2024-06-03 18:21:47 +0500

    ADD - Explanation for the FrequencyCounter

--- frequencyCounter/index.js
+++ frequencyCounter/index.js
@@ -39,3 +39,9 @@ const same = (array1, array2) => {
 console.log("same([1, 2, 3], [4, 1, 9]))", same([1, 2, 3], [4, 1, 9])); // true
 console.log("same([1, 2, 3], [1, 9])", same([1, 2, 3], [1, 9])); // false
 console.log("same([1, 2, 1], [4, 4, 1])", same([1, 2, 1], [4, 4, 1])); // false
+
+// Explanation:
+// 1. Check if the length of both arrays is same, if its not then its a default false;
+// 2. Create two objects using both arrays, where the key is the value of the array and the value of the number of times it occurs in that array.
+// 3. Itterate over the first object's keys and check if the squared value of the key is present in the second object as well as if the number of times occurs is same.
+// 4. If any of the above conditions fail, return false otherwise return true.
