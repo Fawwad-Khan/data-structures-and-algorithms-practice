2a1eb05598b8ff2c5a5af98797b2c3a0fb435180
Fawwad-Khan <fawwad.j4k@gmail.com>
2024-06-04 18:16:23 +0500

    ADD - Runs for maxSubArraySum

--- maxSubArraySum/index.js
+++ maxSubArraySum/index.js
@@ -32,3 +32,9 @@ const maxSubArraySum = (array, num) => {
   }
   return maxSum;
 };
+
+console.log(maxSubArraySum([1, 2, 5, 2, 8, 1, 5], 2)); // 10
+console.log(maxSubArraySum([1, 2, 5, 2, 8, 1, 5], 4)); // 17
+console.log(maxSubArraySum([4, 2, 1, 6], 1)); // 6
+console.log(maxSubArraySum([4, 2, 1, 6, 2], 4)); // 13
+console.log(maxSubArraySum([], 4)); // null
